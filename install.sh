68d45d60402278e8b4c754cb80a7b0ebcd57bb8c
James Langille <james.langille@shopify.com>
2021-02-11 13:30:12 -0500

    Initial attempt at dotfiles

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# fzf
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Syntax highlighting
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+# Install files from $HOME/dotfiles to their correct locations
+dot="$HOME/dotfiles"
+cp "$dot/.vimrc" $HOME
+cp "$dot/.aliases" $HOME
+cp "$dot/.zshrc" $HOME
+cp "$dot/.tmux.conf" $HOME
+cp "$dot/.gitconfig" $HOME
+cp "$dot/.gitignore" $HOME
+
